a39fdfa8cc7734e141a2332a96c6f1f764302705
Code4Daze <jevon.fillette@gmail.com>
2022-07-29 14:58:33 -0400

    Add content to home tab and change tab font

--- src/home.js
+++ src/home.js
@@ -2,9 +2,19 @@ function loadHome() {
   let main = document.querySelector('main');
 
   let welcome = document.createElement('p');
+  welcome.classList.add('welcome');
   welcome.textContent = "Welcome to Blue Sage!"
 
+  let outsidePic = document.createElement('div');
+  outsidePic.classList = 'outsidePic';
+
+  let welcome2 = document.createElement('p');
+  welcome2.classList.add('welcome2');
+  welcome2.textContent = "We are purposefully plant based and vegetarian by chance.  Come join us for unique, modern vegetarian dishes and desserts!"
+
   main.appendChild(welcome);
+  main.appendChild(outsidePic);
+  main.appendChild(welcome2);
 
   return main;
 }

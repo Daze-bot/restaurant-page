422ed14eb25253eb122bef19f342f1f040e546b7
Code4Daze <jevon.fillette@gmail.com>
2022-07-28 23:45:41 -0400

    Refactor intial code into a new js file

--- src/index.js
+++ src/index.js
@@ -1,44 +1,6 @@
 import './style.css';
-
-function createHeader() {
-  let pageHeader = document.createElement('header');
-
-  let h1 = document.createElement('h1');
-  h1.textContent = "Blue Sage";
-  pageHeader.appendChild(h1);
-
-  let text = document.createElement('p');
-  text.textContent = "VEGETARIAN GRILLE";
-  pageHeader.appendChild(text);
-
-  return pageHeader;
-}
-
-function createTabs() {
-  let pageTabs = document.createElement('div');
-  pageTabs.classList.add('pageTabs');
-
-  let homeBtn = document.createElement('button');
-  homeBtn.classList.add('homeBtn');
-  homeBtn.textContent = "Home";
-
-  let menuBtn = document.createElement('button');
-  menuBtn.classList.add('menuBtn');
-  menuBtn.textContent = "Menu";
-
-  let contactBtn = document.createElement('button');
-  contactBtn.classList.add('contactBtn');
-  contactBtn.textContent = "Contact";
-
-  pageTabs.appendChild(homeBtn);
-  pageTabs.appendChild(menuBtn);
-  pageTabs.appendChild(contactBtn);
-
-  return pageTabs;
-}
-
-let pageContent = document.createElement('main');
+import {createHeader, createTabs, createMain} from './initLoad';
 
 document.body.appendChild(createHeader());
-document.body.appendChild(createTabs());
-document.body.appendChild(pageContent);
+document.body.appendChild(createTabs().pageTabs);
+document.body.appendChild(createMain());
---
+++ src/initLoad.js
@@ -0,0 +1,48 @@
+function createHeader() {
+  let pageHeader = document.createElement('header');
+
+  let h1 = document.createElement('h1');
+  h1.textContent = "Blue Sage";
+  pageHeader.appendChild(h1);
+
+  let text = document.createElement('p');
+  text.textContent = "VEGETARIAN GRILLE";
+  pageHeader.appendChild(text);
+
+  return pageHeader;
+}
+
+function createTabs() {
+  let pageTabs = document.createElement('div');
+  pageTabs.classList.add('pageTabs');
+
+  let homeBtn = document.createElement('button');
+  homeBtn.classList.add('homeBtn');
+  homeBtn.textContent = "Home";
+
+  let menuBtn = document.createElement('button');
+  menuBtn.classList.add('menuBtn');
+  menuBtn.textContent = "Menu";
+
+  let contactBtn = document.createElement('button');
+  contactBtn.classList.add('contactBtn');
+  contactBtn.textContent = "Contact";
+
+  pageTabs.appendChild(homeBtn);
+  pageTabs.appendChild(menuBtn);
+  pageTabs.appendChild(contactBtn);
+
+  return {pageTabs, homeBtn, menuBtn, contactBtn};
+}
+
+function createMain() {
+  let pageContent = document.createElement('main');
+
+  return pageContent;
+}
+
+export {
+  createHeader,
+  createTabs,
+  createMain
+};
